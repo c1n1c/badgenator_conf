d5ebe3d487f9b95e8616ff63145019323c58701c
Simeon Movchan <simeon.movchan@gmail.com>
2013-04-22 18:09:51 +0600

    Add migrations for badges and badge_sets (fixed #1).

--- Gemfile
+++ Gemfile
@@ -2,7 +2,8 @@ source 'https://rubygems.org'
 
 gem 'rails', '3.2.8'
 
-gem 'sqlite3'
+gem 'pg'
+gem 'foreigner'
 
 gem 'jquery-rails'
 gem 'haml'
---
+++ db/migrate/20130422104629_create_badges.rb
@@ -0,0 +1,16 @@
+class CreateBadges < ActiveRecord::Migration
+  def change
+    create_table :badges do |t|
+      t.string :name, null: false
+      t.string :surname
+      t.string :company
+      t.string :profession
+      
+      t.references :badge_set, foreign_key: true
+      
+      t.timestamps
+    end
+    
+    add_index :badges, :badge_set_id
+  end
+end
