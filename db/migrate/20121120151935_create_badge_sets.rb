e7b83c5af0ed574de85bac5533119376e9f68729
Simeon Movchan <simeon.movchan@gmail.com>
2013-04-23 17:23:00 +0600

    BadgeSet/Badge associations, models/migrations clean, fixed #3.

--- db/migrate/20121120151935_create_badge_sets.rb
+++ db/migrate/20121120151935_create_badge_sets.rb
@@ -1,8 +1,11 @@
+# -*- coding: utf-8 -*-
+
 class CreateBadgeSets < ActiveRecord::Migration
   def change
     create_table :badge_sets do |t|
-      t.string :name, null: false
+      t.string :name, :null => false
       t.string :image
+
       t.timestamps
     end
   end
